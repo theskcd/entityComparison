a01e9deb329374342d1010100d5c5ae6eca3f89c
Naresh <ghostwriternr@gmail.com>
2016-10-12 15:32:06 +0530

    Zombiejs (fails)

--- app/routes/APIRouter.js
+++ app/routes/APIRouter.js
@@ -1,130 +1,163 @@
-var express=require('express');
-var router=express.Router();
-var request=require('request');
-var cheerio=require('cheerio');
-var fs=require('fs');
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var Browser = require('zombie');
+var assert = require("assert");
 
-router.use(function(req,res,next){
-	next();
+router.use(function(req, res, next) {
+    next();
 });
 
 router.route('/getDataAnimals')
-	.get(function(req,res){
-		var siteUrl="https://en.wikipedia.org/wiki/List_of_animals_by_common_name";
-		var systemProxy="htttp://10.3.100.207:8080";
-		console.log(siteUrl);
-		request({url:siteUrl,proxy:systemProxy},function(error,responses,html){
-			if(!error){
-				console.log('no error');
-				var $=cheerio.load(html);
-				$('.mw-body-content').each(function(i,element){
-					var data=$(this);
-					for (var i = 0; i <= data.children()[3].children.length - 1; i++) {
-						if(data.children()[3].children[i].attribs!=undefined && 
-							data.children()[3].children[i].attribs.class=="div-col columns column-width"){
-							// console.log("class is written as " + data.children()[3].children[i].attribs.class);
-							for(var j=0;j<data.children()[3].children[i].children.length;j++){
-								if(data.children()[3].children[i].children[j]!=undefined &&
-									data.children()[3].children[i].children[j].name!=undefined &&  
-									data.children()[3].children[i].children[j].name=="ul"){
-									// console.log(data.children()[3].children[i].children[j].children.length);
-									if(data.children()[3].children[i].children[j].children[0].parent!=undefined){
-										// console.log(data.children()[3].children[i].children[j].children[0].parent);
-										if(data.children()[3].children[i].children[j].children[0].parent.children!=undefined){
-											for(var k=0;k<data.children()[3].children[i].children[j].children[0].parent.children.length;k++){
-												if(data.children()[3].children[i].children[j].children[0].parent.children[k].type=="tag"){
-													console.log(data.children()[3].children[i].children[j].children[0].parent.children[k].children[0].attribs);
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					};
-				})
-			}
-			else{
-				console.log(error+' stupid error!');
-			}
-		});
-	});
+    .get(function(req, res) {
+        var siteUrl = "https://en.wikipedia.org/wiki/List_of_animals_by_common_name";
+        var systemProxy = "htttp://10.3.100.207:8080";
+        console.log(siteUrl);
+        request({
+            url: siteUrl,
+            proxy: systemProxy
+        }, function(error, responses, html) {
+            if (!error) {
+                console.log('no error');
+                var $ = cheerio.load(html);
+                $('.mw-body-content').each(function(i, element) {
+                    var data = $(this);
+                    for (var i = 0; i <= data.children()[3].children.length - 1; i++) {
+                        if (data.children()[3].children[i].attribs != undefined &&
+                            data.children()[3].children[i].attribs.class == "div-col columns column-width") {
+                            // console.log("class is written as " + data.children()[3].children[i].attribs.class);
+                            for (var j = 0; j < data.children()[3].children[i].children.length; j++) {
+                                if (data.children()[3].children[i].children[j] != undefined &&
+                                    data.children()[3].children[i].children[j].name != undefined &&
+                                    data.children()[3].children[i].children[j].name == "ul") {
+                                    // console.log(data.children()[3].children[i].children[j].children.length);
+                                    if (data.children()[3].children[i].children[j].children[0].parent != undefined) {
+                                        // console.log(data.children()[3].children[i].children[j].children[0].parent);
+                                        if (data.children()[3].children[i].children[j].children[0].parent.children != undefined) {
+                                            for (var k = 0; k < data.children()[3].children[i].children[j].children[0].parent.children.length; k++) {
+                                                if (data.children()[3].children[i].children[j].children[0].parent.children[k].type == "tag") {
+                                                    console.log(data.children()[3].children[i].children[j].children[0].parent.children[k].children[0].attribs);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    };
+                })
+            } else {
+                console.log(error + ' stupid error!');
+            }
+        });
+    });
 
 router.route('/getDataPlants')
-	.get(function(req,res){
-		var siteUrl="https://en.wikipedia.org/wiki/List_of_plants_by_common_name";
-		var systemProxy="htttp://10.3.100.207:8080";
-		console.log(siteUrl);
-		request({url:siteUrl,proxy:systemProxy},function(error,responses,html){
-			if(!error){
-				console.log('no error');
-				var $=cheerio.load(html);
-				$('.mw-body-content').each(function(i,element){
-					var data=$(this);
-					for (var i = 0; i <= data.children()[3].children.length - 1; i++) {
-						if(data.children()[3].children[i].type!=undefined){
-							if(data.children()[3].children[i].type=="tag"){
-								if(data.children()[3].children[i].name=="ul" && data.children()[3].children[i].children!=undefined){
-									for(var j=0;j<data.children()[3].children[i].children.length;j++){
-										if(data.children()[3].children[i].children[j].name!=undefined && 
-											data.children()[3].children[i].children[j].name=="li"){
-											for(var k=0;k<data.children()[3].children[i].children[j].children.length;k++){
-												if(data.children()[3].children[i].children[j].children[k].type!=undefined && 
-													data.children()[3].children[i].children[j].children[k].type=="tag"){
-													if(data.children()[3].children[i].children[j].children[k].attribs!=undefined){
-														if(data.children()[3].children[i].children[j].children[k].attribs.href!=undefined){
-															console.log(data.children()[3].children[i].children[j].children[k].attribs);
-														}
-													}
-													for(var l=0;l<data.children()[3].children[i].children[j].children[k].children.length;l++){
-														if(data.children()[3].children[i].children[j].children[k].children[l].type!=undefined &&
-															data.children()[3].children[i].children[j].children[k].children[l].type=="tag"){
-															if(data.children()[3].children[i].children[j].children[k].children[l].attribs!=undefined){
-																if(data.children()[3].children[i].children[j].children[k].children[l].attribs.href!=undefined){
-																	console.log(data.children()[3].children[i].children[j].children[k].children[l].attribs);
-																}
-															}
-														}
-													}
-												}
-											}
-										}
-									}
-								}
-								else if(data.children()[3].children[i].name=="dl"){
-									for(var j=0;j<data.children()[3].children[i].children.length;j++){
-										if(data.children()[3].children[i].children[j].type!=undefined &&
-											data.children()[3].children[i].children[j].type=="tag"){
-											for(var k=0;k<data.children()[3].children[i].children[j].children.length;k++){
-												if(data.children()[3].children[i].children[j].children[k].type!=undefined &&
-													data.children()[3].children[i].children[j].children[k].type=="tag"){
-													for(var l=0;l<data.children()[3].children[i].children[j].children[k].children.length;l++){
-														if(data.children()[3].children[i].children[j].children[k].children[l].type!=undefined && 
-															data.children()[3].children[i].children[j].children[k].children[l].type=="tag"){
-															for(var m=0;m<data.children()[3].children[i].children[j].children[k].children[l].children.length;m++){
-																if(data.children()[3].children[i].children[j].children[k].children[l].children[m].type!=undefined && 
-																	data.children()[3].children[i].children[j].children[k].children[l].children[m].type=="tag"){
-																	if(data.children()[3].children[i].children[j].children[k].children[l].children[m].children[0].attribs!=undefined){
-																		console.log(data.children()[3].children[i].children[j].children[k].children[l].children[m].children[0].attribs);
-																	}
-																}
-															}
-														}
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					};
-				})
-			}
-			else{
-				console.log(error+' stupid error!');
-			}
-		});
-	})
+    .get(function(req, res) {
+        var siteUrl = "https://en.wikipedia.org/wiki/List_of_plants_by_common_name";
+        var systemProxy = "htttp://10.3.100.207:8080";
+        console.log(siteUrl);
+        request({
+            url: siteUrl,
+            proxy: systemProxy
+        }, function(error, responses, html) {
+            if (!error) {
+                console.log('no error');
+                var $ = cheerio.load(html);
+                $('.mw-body-content').each(function(i, element) {
+                    var data = $(this);
+                    for (var i = 0; i <= data.children()[3].children.length - 1; i++) {
+                        if (data.children()[3].children[i].type != undefined) {
+                            if (data.children()[3].children[i].type == "tag") {
+                                if (data.children()[3].children[i].name == "ul" && data.children()[3].children[i].children != undefined) {
+                                    for (var j = 0; j < data.children()[3].children[i].children.length; j++) {
+                                        if (data.children()[3].children[i].children[j].name != undefined &&
+                                            data.children()[3].children[i].children[j].name == "li") {
+                                            for (var k = 0; k < data.children()[3].children[i].children[j].children.length; k++) {
+                                                if (data.children()[3].children[i].children[j].children[k].type != undefined &&
+                                                    data.children()[3].children[i].children[j].children[k].type == "tag") {
+                                                    if (data.children()[3].children[i].children[j].children[k].attribs != undefined) {
+                                                        if (data.children()[3].children[i].children[j].children[k].attribs.href != undefined) {
+                                                            console.log(data.children()[3].children[i].children[j].children[k].attribs);
+                                                        }
+                                                    }
+                                                    for (var l = 0; l < data.children()[3].children[i].children[j].children[k].children.length; l++) {
+                                                        if (data.children()[3].children[i].children[j].children[k].children[l].type != undefined &&
+                                                            data.children()[3].children[i].children[j].children[k].children[l].type == "tag") {
+                                                            if (data.children()[3].children[i].children[j].children[k].children[l].attribs != undefined) {
+                                                                if (data.children()[3].children[i].children[j].children[k].children[l].attribs.href != undefined) {
+                                                                    console.log(data.children()[3].children[i].children[j].children[k].children[l].attribs);
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                } else if (data.children()[3].children[i].name == "dl") {
+                                    for (var j = 0; j < data.children()[3].children[i].children.length; j++) {
+                                        if (data.children()[3].children[i].children[j].type != undefined &&
+                                            data.children()[3].children[i].children[j].type == "tag") {
+                                            for (var k = 0; k < data.children()[3].children[i].children[j].children.length; k++) {
+                                                if (data.children()[3].children[i].children[j].children[k].type != undefined &&
+                                                    data.children()[3].children[i].children[j].children[k].type == "tag") {
+                                                    for (var l = 0; l < data.children()[3].children[i].children[j].children[k].children.length; l++) {
+                                                        if (data.children()[3].children[i].children[j].children[k].children[l].type != undefined &&
+                                                            data.children()[3].children[i].children[j].children[k].children[l].type == "tag") {
+                                                            for (var m = 0; m < data.children()[3].children[i].children[j].children[k].children[l].children.length; m++) {
+                                                                if (data.children()[3].children[i].children[j].children[k].children[l].children[m].type != undefined &&
+                                                                    data.children()[3].children[i].children[j].children[k].children[l].children[m].type == "tag") {
+                                                                    if (data.children()[3].children[i].children[j].children[k].children[l].children[m].children[0].attribs != undefined) {
+                                                                        console.log(data.children()[3].children[i].children[j].children[k].children[l].children[m].children[0].attribs);
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    };
+                })
+            } else {
+                console.log(error + ' stupid error!');
+            }
+        });
+    });
 
-module.exports=router;
+router.route('/testZombie')
+    .get(function(req, res) {
+        Browser.localhost('https://www.ncbi.nlm.nih.gov/', 3000);
+
+        describe('Visit ncbi taxonomy page', function() {
+            const browser = new Browser();
+            before(function(done) {
+                browser.visit('/taxonomy', done);
+            });
+
+            describe('perform search', function() {
+                before(function() {
+                    browser.
+                    fill('#term', 'Lion');
+                    return browser.pressButton('#search', done);
+                });
+
+                describe('should be successful', function() {
+                    browser.assert.success();
+                });
+
+                describe('should see results page', function() {
+                    browser.assert.text('title', 'Panthera leo - Taxonomy - NCBI');
+                });
+            });
+        });
+    });
+
+module.exports = router;
